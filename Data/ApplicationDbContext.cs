5e461ae05670489b95550e1bdcfc27e6862fe816
Daniel Gherghetta <gherghetta@gmail.com>
2024-10-04 11:51:22 +0200

    Changed the DbContext to use our domain, and our custom user class

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-
+using MvcWithIdentityAndEFCore.Models;
 namespace MvcWithIdentityAndEFCore.Data;
 
-public class ApplicationDbContext : IdentityDbContext
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
+    // DbSet för din applikationsdata
+    public DbSet<Event> Events { get; set; }
+
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Event>()
+            .HasMany(e => e.EventParticipants)
+            .WithMany(u => u.Events)
+            .UsingEntity(j => j.ToTable("EventParticipants"));
     }
 }
