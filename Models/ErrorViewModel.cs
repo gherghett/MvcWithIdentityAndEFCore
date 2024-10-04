5e252d38f67de249f3c40dae27b464a6c75633bd
Daniel Gherghetta <gherghetta@gmail.com>
2024-10-04 11:44:37 +0200

    Initial created w: dotnet new mvc --auth Individual

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MvcWithIdentityAndEFCore.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
